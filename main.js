7a271d78614653021ca3b3ec31366e37c4545c05
Konstantin <pukkikonstantin@gmail.com>
2019-11-17 20:46:27 +0300

    Рабочая версия 1

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
+class InputRange {
+    constructor(id, discription) {
+        this.discription = discription;
+
+        this.elem = document.getElementById(id);
+        this.value = this.elem.value;
+
+        this.inscription = document.createElement('div');
+        this.inscription.classList = 'inscription';
+        this.inscription.innerHTML = this.elem.value + this.discription;
+
+
+        this.elem.parentNode.appendChild(this.inscription);
+
+        this.elem.oninput = () => {
+            this.inscription.innerHTML = this.elem.value + this.discription;
+            this.value = this.elem.value;
+        };
+    }
+
+    get getValue() {
+        return this.value;
+    }
+
+    stop() {
+        this.elem.oninput = null;
+        this.inscription.style.color = '#4cd137';
+        this.inscription.style.fontWeight = 'bold';
+    }
+}
+
 let canvas = document.getElementById('canvas');
 
-
-let w = canvas.width = window.innerWidth / 2 - window.innerWidth / 2 % 10;
+let w = canvas.width = (window.innerWidth / 2 - window.innerWidth / 2 % 10 > window.innerHeight) 
+                        ?  window.innerHeight - window.innerHeight % 10
+                        : window.innerWidth / 2 - window.innerWidth / 2 % 10;
 let h = canvas.height = w;
 
 let ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
 
-let sizeBlock = 5;
-
-let N = w / sizeBlock;
-let M = h / sizeBlock;
+let N = w / inpSize.getValue;
+let M = N;
 let arr = [];
 
 for (let i = 0; i < N; i++) {
@@ -19,23 +49,33 @@ for (let i = 0; i < N; i++) {
     }
 }
 
+let inpSize = new InputRange('inpSize', 'px');
+let inpSpeed = new InputRange('inpSpeed', 'fps');
+
+document.getElementById('btnStart').addEventListener('click', start);
+
 function start() {
     document.getElementById('btnStart').removeEventListener('click', start);
+    canvas.onmousedown = null;
+
+    N = w / inpSize.getValue;
+    M = N;
+
+    inpSize.stop();
+    inpSpeed.stop();
 
     setInterval(() => {
         ctx.clearRect(0, 0, w, h);
         life();
         drawPixel();
-    }, 1000 / 60);
+    }, 1000 / inpSpeed.getValue);
 }
-
-document.getElementById('btnStart').addEventListener('click', start);
 
 function drawPixel() {
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < M; j++) {
             if (arr[i][j]) {
-                ctx.fillRect(i * sizeBlock, j * sizeBlock, sizeBlock, sizeBlock);
+                ctx.fillRect(i * inpSize.getValue, j * inpSize.getValue, inpSize.getValue, inpSize.getValue);
             }
         }
     }
@@ -102,6 +142,8 @@ function copyArray() {
 }
 
 canvas.onmousedown = (e) => {
+    inpSize.stop();
+
     setPixel(e.clientX, e.clientY);
     canvas.onmousemove = (event) => {
         setPixel(event.clientX, event.clientY);
@@ -114,31 +156,14 @@ canvas.onmouseup = () => {
 
 function setPixel(x, y) {
     let rect = canvas.getBoundingClientRect();
-    let i = Math.floor((x - rect.left) / sizeBlock);
-    let j = Math.floor((y - rect.top) / sizeBlock);
+    let i = Math.floor((x - rect.left) / inpSize.getValue);
+    let j = Math.floor((y - rect.top) / inpSize.getValue);
 
     arr[i][j] = 1;
-
+    ctx.clearRect(0, 0, w, h);
     drawPixel();
 }
 
-class InputRange {
-    constructor(id) {
-        this.elem = document.getElementById(id);
 
-        this.inscription = document.createElement('div');
-        this.inscription.classList = 'inscription';
-        this.changeRange();
 
-        this.elem.parentNode.appendChild(this.inscription);
-
-        this.elem.addEventListener('input', this.changeRange.bind(this));
-    }
-
-    changeRange() {
-        this.inscription.innerHTML = this.elem.value;
-    }
-}
-new InputRange('inpSize');
-new InputRange('inpSpeed');
 
